82448d6b6b0c9979150e80a032e0b789e781cc50
hira22 <masayuki.hiraoka@ietty.co.jp>
2021-02-03 11:06:25 +0900

    Displaying Data in a List

--- Scrumdinger/DailyScrum.swift
+++ Scrumdinger/DailyScrum.swift
@@ -7,11 +7,22 @@
 
 import SwiftUI
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    // MARK: - Identifiable
+    let id: UUID
+    
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var color: Color
+    
+    internal init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, color: Color) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.color = color
+    }
 }
 
 extension DailyScrum {

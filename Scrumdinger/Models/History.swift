77870e743f1b49d1ded742738da8eb8677c3cc52
hira22 <masayuki.hiraoka@ietty.co.jp>
2021-02-13 08:35:01 +0900

    Transcribing Speech to Text

--- Scrumdinger/Models/History.swift
+++ Scrumdinger/Models/History.swift
@@ -8,17 +8,19 @@
 import Foundation
 
 struct History: Identifiable, Codable {
-    internal init(id: UUID = UUID(), date: Date = Date(), attendees: [String], lengthInMinutes: Int) {
+    internal init(id: UUID = UUID(), date: Date = Date(), attendees: [String], lengthInMinutes: Int, transcript: String? = nil) {
         self.id = id
         self.date = date
         self.attendees = attendees
         self.lengthInMinutes = lengthInMinutes
+        self.transcript = transcript
     }
     
     let id: UUID
     let date: Date
     let attendees: [String]
     let lengthInMinutes: Int
+    var transcript: String?
     
     
 }
